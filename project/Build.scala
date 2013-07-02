e97217a7a1e6ab708f6b121b937486c83b867407
zhangpeng <zhangpdg@gmail.com>
2013-07-02 22:40:16 +0800

    page current ok

--- project/Build.scala
+++ project/Build.scala
@@ -7,14 +7,16 @@ object ApplicationBuild extends Build {
     val appName         = "howtoforge"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
+	val appDependencies = Seq(
       // Add your project dependencies here,
-      libraryDependencies += "org.jsoup" % "jsoup" % "1.7.2",
+      "org.jsoup" % "jsoup" % "1.7.2",
       "mysql" % "mysql-connector-java" % "5.1.18"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
     )
+	
+    
 
 }
